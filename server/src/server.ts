ffa597eff3626205100f80dbaa2102745030d33f
Dei Valko <xp.gram@gmail.com>
2022-09-07 17:22:50 -0700

    add note

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import * as Express from "express";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 
-const PORT = process.env.PORT || 3001;
+const PORT = Number(process.env.PORT || 3001);
 
 const app = Express();
 app.set("port", PORT);
 
 const http = require('http').Server(app);   // TODO Make this an import
-const io = require('socket.io') (http);     // TODO Make this an import
+const io = new Server(http);
 
 // simple '/' endpoint sending a Hello World response
 // app.get("/", (req: any, res: any) => {
@@ -22,6 +22,12 @@ io.on("connection", socket => {
     console.log(`disconnected ${socket.id} : ${reason}`);
   })
 })
+
+// io.listen(PORT);
+
+// TODO Game.online is configured to initialize a socket.io object which.. *should* be connecting
+// to localhost:3001, so I'm just trying to connect the two instances.
+// The game runs on webpack-dev-server, so I may need to proxy a request or something.
 
 // start our simple server up on localhost:3000
 const server = http.listen(PORT, () => {
