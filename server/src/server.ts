fc2a246a213f21a8255e5fc5bb9b1ef1eeafbb97
Dei Valko <xp.gram@gmail.com>
2022-09-07 17:38:31 -0700

    add note

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
-import * as Express from "express";
+// import * as Express from "express";
 import { Server } from "socket.io";
 
 const PORT = Number(process.env.PORT || 3001);
 
-const app = Express();
-app.set("port", PORT);
+// const app = Express();
+// app.set("port", PORT);
 
-const http = require('http').Server(app);   // TODO Make this an import
-const io = new Server(http);
+// const http = require('http').Server(app);   // TODO Make this an import
+const io = new Server(PORT);
+console.log(`listening on *:${PORT}`);
 
 // simple '/' endpoint sending a Hello World response
 // app.get("/", (req: any, res: any) => {
@@ -28,8 +29,10 @@ io.on("connection", socket => {
 // TODO Game.online is configured to initialize a socket.io object which.. *should* be connecting
 // to localhost:3001, so I'm just trying to connect the two instances.
 // The game runs on webpack-dev-server, so I may need to proxy a request or something.
+//
+// Ohhhhh this is a CORS problem. Okay, well I'll do that later.
 
 // start our simple server up on localhost:3000
-const server = http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
-});
+// const server = http.listen(PORT, () => {
+//   console.log(`listening on *:${PORT}`);
+// });
