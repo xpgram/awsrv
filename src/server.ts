d8423884b4692f96914ffab730414b117b75496e
Dei Valko <xp.gram@gmail.com>
2022-09-14 16:45:36 -0700

    remove comments / test github webhook

--- src/server.ts
+++ src/server.ts
@@ -145,11 +145,6 @@ io.on("connect", async socket => {
   // TODO Do this by matching a user auth to a user in the Db; Do this on 'game join', not 'connection'.
   io.to(socket.id).emit('game session data', metrics.clients_connected - 1);
 
-  // socket.join(`game_${gameId}`);  // This will be useful later, when GameId becomes the name of a room.
-  // Every socket, by default, is a member of its own room. This is how DMs can work.
-  // But other room strings can also be used. So, an ID string prepended with 'game_' will be
-  // a place that players of some game session can talk to each other.
-
   // TODO Signal relay between clients. Is there a more compact way to do this?
   // I want something like:
   //  broadcastEvents = ['troop order', 'turn change'];
@@ -157,10 +152,12 @@ io.on("connect", async socket => {
   //    if (broadcastEvents.includes(ev))
   //      socket.broadcast.emit(ev, data);
   //  })
+
   socket.on("troop order", data => {
     console.log(`game: instruction ${JSON.stringify(data)}`);
     socket.broadcast.emit("troop order", data); // This sends the message to everyone but self, correct?
   })
+  
   socket.on("turn change", () => {
     console.log(`game: turn change`);
     socket.broadcast.emit("turn change", undefined);
